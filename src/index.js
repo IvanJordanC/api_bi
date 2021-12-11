8cf53c99a91f014cef92a0a08c87494ee9689856
IvanJordanC <ijjordanc@utn.edu.ec>
2021-12-11 10:38:04 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+//Puerto
+const PORT =    process.env.PORT || 3000;
+
 //Routes
 app.use(require('./routes/index'));
 
-app.listen(3000);
+app.listen(PORT);
 console.log('Server on port 3000');
